a1029867473b844b614486352b5a900488c58634
Clemens Solar <solar.c@gmx.net>
2022-01-01 21:35:47 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# zamg-measurment-collector
+
+This repository collects the raw hourly ZAMG weather data and commits them to this repository.
---
+++ bin/collect.js
@@ -0,0 +1,48 @@
+'use strict';
+const {Zamg} = require('../');
+const {access, readFile, writeFile} = require('fs/promises');
+
+
+
+(async() => {
+  const zamg = new Zamg();
+
+  console.log('Request current ZAMG measurements.');
+
+  let result = await zamg.get();
+  let storeData = true;
+  let storecsv = result.responseBody;
+
+  // TODO Extract date from result
+
+  const fileName = `data/${result.parsed[0].Datum}.csv`;
+  let exists = false;
+
+  try {
+    await access(fileName);
+    exists = true;
+  } catch (err) {
+    // Do nothing except log that the file is not existing.
+    console.log(`${fileName} is not existing.`);
+  }
+
+  // If the file already exists, we want to remove the first line.
+  if (exists) {
+    storecsv = storecsv.split('\n');
+    storecsv.shift();
+    storecsv = storecsv.join('\n');
+
+    // Check if the data is already existing in the target file
+    const current = await readFile(fileName);
+
+    if (current.toString().indexOf(storecsv) !== -1) {
+      storeData = false;
+      console.log('Data is already existing, no need to store');
+    }
+  }
+
+  if (storeData) {
+    await writeFile(fileName, storecsv, {flag: 'a'});
+  }
+
+})();
